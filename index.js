fbe2f85320726ce96e9614198e14c11a035da599
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-07-12 23:31:36 -0300

    Criando array de produtos

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const productsCart = [
     {
         id: 1,
-        name: "",
-        price: 0.00
-    }
+        name: "Uva Crimson",
+        price: 44.99
+    },
+    {
+        id: 2,
+        name: "Vinho Canção",
+        price: 17.98
+    },
+    {
+        id: 3,
+        name: "Água de coco",
+        price: 8.99
+    },
+    {
+        id: 4,
+        name: "Mamão",
+        price: 9.98
+    },  
+    {
+        id: 5,
+        name: "Água tônica",
+        price: 17.98
+    }  
 ]
 let Body = document.querySelector("body");
 let Main = document.createElement("main");
