662ad577b9b903c280888ec5aa85847879b88415
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-07-12 23:26:50 -0300

    Add elementos básicos pelo DOM

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ const productsCart = [
         price: 0.00
     }
 ]
+let Body = document.querySelector("body");
+let Main = document.createElement("main");
+let productList = document.createElement("ul");
+let productDetails = document.createElement("li");
+let buttonEnd = document.createElement("button");
+
+Body.appendChild(Main);
+Main.appendChild(productList);
+Main.appendChild(productDetails);
